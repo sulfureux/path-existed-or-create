6240105d9e05efe084bd9913c7c8cd0f2b6bcba6
Pierre Neter <pierreneter@gmail.com>
2019-09-20 00:05:29 +0700

    support for nodejs version < v10.12.0

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const fs = require("fs");
+var fs = require("fs");
+var compareVersions = require('compare-versions');
 
 function pathExistedOrCreate(path) {
   try {
@@ -8,4 +9,25 @@ function pathExistedOrCreate(path) {
   }
 }
 
-module.exports = pathExistedOrCreate;
+function pathExistedOrCreateSupportNodeVersionLesser10(path) {
+  var paths = path.split("/");
+  var length = paths.length;
+  var currentPath = "";
+
+  for (var i = 0; i < length; i++) {
+    currentPath = `${currentPath}/${paths[i]}`;
+    try {
+      fs.accessSync(currentPath, fs.F_OK);
+    } catch (e) {
+      fs.mkdirSync(currentPath);
+    }
+  }
+}
+
+var exportFn = pathExistedOrCreate;
+
+if (compareVersions("v10.12.0", process.version) >= 0) {
+  exportFn = pathExistedOrCreateSupportNodeVersionLesser10;
+}
+
+module.exports = exportFn;
